afb4e8618e3252ce43548f1badec554dda4f16e9
chackochii <163020056+chackochii@users.noreply.github.com>
2025-12-05 02:40:26 +0530

    Update topbar.js

--- app/components/topbar.js
+++ app/components/topbar.js
@@ -1,16 +1,21 @@
 "use client";
 import { FiSearch, FiPlus } from "react-icons/fi";
+import { useState, useEffect } from "react";
 
 export default function Topbar() {
+  const [user, setUser] = useState(null);
+
+  const username = user?.name;
+
+  useEffect(() => {
+    const saved = localStorage.getItem("employeeUser");
+    if (saved) setUser(JSON.parse(saved));
+  }, []);
+
   return (
     <header className="bg-slate-900 border-b border-slate-800 px-4">
-
       {/* ---------- TOP ROW (Always visible on all screens) ---------- */}
       <div className="flex items-center justify-between md:justify-start gap-4">
-
-        {/* Project Title */}
-
-
         {/* Desktop Search (hidden on mobile) */}
         <div className="hidden md:block flex-1 max-w-3xl mx-auto">
           <div className="relative">
@@ -24,32 +29,17 @@ export default function Topbar() {
 
         {/* Desktop Create Button + Avatar (hidden on mobile) */}
         <div className="hidden md:flex items-center gap-4">
-          {/* <button className="flex items-center gap-2 bg-indigo-600 hover:bg-indigo-500 px-3 py-2 rounded-md text-white">
-            <FiPlus />
-            <span>Create Issue</span>
-          </button> */}
-
-          <div className="w-9 h-9 rounded-full bg-slate-700" />
+          <div className="w-8 h-8 rounded-full bg-indigo-600">
+            <div className="w-9 h-9 flex items-center justify-center rounded-full bg-indigo-600 text-white font-semibold uppercase">
+              {username
+                ?.split(" ")
+                ?.map((word) => word[0])
+                ?.join("")
+                ?.slice(0, 2) || ""}
+            </div>
+          </div>
         </div>
       </div>
-
-      {/* ---------- MOBILE ONLY SECTION ---------- */}
-      {/* <div className="mt-3 flex flex-col gap-3 md:hidden"> */}
-
-        {/* Mobile Search */}
-        {/* <div className="relative">
-          <input
-            placeholder="Search..."
-            className="w-full bg-slate-800 border border-slate-800 rounded-lg px-4 py-2 focus:outline-none"
-          />
-          <FiSearch className="absolute top-2.5 right-3 text-slate-400" />
-        </div>
-
-        <button className="flex items-center justify-center gap-2 bg-indigo-600 hover:bg-indigo-500 px-4 py-2 rounded-md text-white">
-          <FiPlus />
-          <span>Create Issue</span>
-        </button> */}
-      {/* </div> */}
     </header>
   );
 }
